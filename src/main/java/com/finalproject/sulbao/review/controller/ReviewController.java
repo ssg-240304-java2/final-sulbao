e650d8c78a32f0f630c358fd4e41ce25eeb907da
lucinda96 <lucinda96@naver.com>
2024-08-19 12:55:16 +0900

    fix: 상품 리뷰 조회, 등록 기능 수정
    - 리뷰 등록일로부터 3일 이내인 경우 수정 버튼 표시
    - order정보 취득 코드 수정

--- src/main/java/com/finalproject/sulbao/review/controller/ReviewController.java
+++ src/main/java/com/finalproject/sulbao/review/controller/ReviewController.java
@@ -1,6 +1,6 @@
 package com.finalproject.sulbao.review.controller;
 
-import com.finalproject.sulbao.cart.dto.OrderItemDTO;
+import com.finalproject.sulbao.cart.dto.OrderDTO;
 import com.finalproject.sulbao.product.model.dto.ProductDTO;
 import com.finalproject.sulbao.review.model.dto.ReviewDTO;
 import com.finalproject.sulbao.review.service.ReviewService;
@@ -29,24 +29,17 @@ public class ReviewController {
 
 
     // 리뷰 등록 페이지 이동
-    @GetMapping("/regist/{orderNo}/{productNo}")
-    public String regist(@PathVariable String orderNo, @PathVariable String productNo, Model model){
-
-        //상품정보 취득
-        ProductDTO product = reviewService.getProductInfo(Long.parseLong(productNo));
+    @GetMapping("/regist/{orderNo}")
+    public String regist(@PathVariable String orderNo, Model model){
 
         //주문정보 취득
-        OrderItemDTO orderItem = reviewService.getOrderInfo(orderNo,productNo);
+        OrderDTO orderDTO = reviewService.getOrderInfo(orderNo);
 
-        // 주문정보에 선택한 상품이 없는경우
-        if(orderItem == null){
-            return "redirect:/review/orderList";
-        }
+        //상품정보 취득
+        ProductDTO product = reviewService.getProductInfo(orderDTO.getOrderItems());
 
+        model.addAttribute("order", orderDTO);
         model.addAttribute("product", product);
-        model.addAttribute("orderItem", orderItem);
-        model.addAttribute("orderNo", orderNo);
-        model.addAttribute("productNo", productNo);
         return "review/regist";
     }
 
@@ -68,6 +61,7 @@ public class ReviewController {
     // 상세조회(리뷰목록에서)
     @GetMapping("/detail/{reviewId}")
     public String detail(@PathVariable Long reviewId, Model model){
+
         ReviewDTO reviewDTO = reviewService.findById(reviewId);
 
         model.addAttribute("review", reviewDTO);
