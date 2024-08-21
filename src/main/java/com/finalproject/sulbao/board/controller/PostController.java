ffbf5120c6bebcc01725179bc03867012f08d394
cylcoder <cylcoder@gmail.com>
2024-08-21 09:17:50 +0900

    Feat: 마이페이지 내가 작성한 게시글 조회 UI 구현

--- src/main/java/com/finalproject/sulbao/board/controller/PostController.java
+++ src/main/java/com/finalproject/sulbao/board/controller/PostController.java
@@ -1,8 +1,11 @@
 package com.finalproject.sulbao.board.controller;
 
+import com.finalproject.sulbao.board.common.SessionHandler;
 import com.finalproject.sulbao.board.dto.PostDto;
+import com.finalproject.sulbao.board.dto.UserDto;
 import com.finalproject.sulbao.board.repository.PostRepository;
 import com.finalproject.sulbao.board.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +19,7 @@ public class PostController {
 
     private final PostService postService;
     private final PostRepository postRepository;
+    private final SessionHandler sessionHandler;
 
     @GetMapping("/admin/board")
     public String admin(Model model) {
@@ -25,7 +29,16 @@ public class PostController {
     }
 
     @GetMapping("/mypage/board")
-    public String user(Model model) {
+    public String user(Model model, HttpServletRequest request) {
+        if (!sessionHandler.isLogin(request)) {
+            return "redirect:/login";
+        }
+
+        UserDto user = sessionHandler.getUserFromSession(request);
+        List<PostDto> posts = postService.findByUser(user.getId());
+
+        model.addAttribute("user", user);
+        model.addAttribute("posts", posts);
         return "board/user/list";
     }
 
