92b2d24f31df9542ec95ce19814b7ae37cc756ac
lucinda96 <lucinda96@naver.com>
2024-08-27 05:08:53 +0900

    fea: 한 술 매거진 관리자/사용자 기능 추가
    - 한 술 매거진 관리자 등록, 수정, 삭제 화면 개발
    - 한 술 매거진 관리자 등록, 수정, 삭제 기능 개발
    - 사용자 목록 및 상세 화면 개발
    - 사용자 한 술 매거진 목록 화면 개발
    - 매거진 기능변경에 의한 entity 변경

---
+++ src/main/java/com/finalproject/sulbao/magazine/repository/MagazineRepository.java
@@ -0,0 +1,14 @@
+package com.finalproject.sulbao.magazine.repository;
+
+
+import com.finalproject.sulbao.product.model.entity.Magazine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MagazineRepository extends JpaRepository<Magazine, Long> {
+
+    @Query(value = "select m from Magazine m where m.displayYn = 'Y' ")
+    List<Magazine> findByDisplayYn();
+}
